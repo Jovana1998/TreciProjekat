fbdc75a13f4058c99577ef8d4774e4fd57428346
JovanaJovic98 <jovic.jovana@elfak.rs>
2021-02-11 11:51:45 +0100

    greske popravljene

--- BazeApoteka/BazeApoteka/Pages/PrepisiRecept.cshtml.cs
+++ BazeApoteka/BazeApoteka/Pages/PrepisiRecept.cshtml.cs
@@ -24,9 +24,23 @@ namespace BazeApoteka.Pages
         public Korisnik pacijent { get; set; }
         [BindProperty]
         public Lek lek { get; set; }
-        public void OnGet()
+       
+        public IActionResult OnGet([FromRoute] String id)
         {
+            Prosledjeno = id; //id recepta
+            var connectionString = "mongodb://localhost/?safe=true";
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase("Apoteka3");
 
+            //ucitamo recepte i nadjemo prosledjen 
+            collection = database.GetCollection<Recept>("recepti");
+            ObjectId iid = ObjectId.Parse(Prosledjeno);
+            recept = collection.Find(x => x.Id == iid).FirstOrDefault();
+
+            Korisnici = database.GetCollection<Korisnik>("korisnici");
+            pacijent = Korisnici.Find(x => x.Id == recept.Pacijent.Id).FirstOrDefault();
+
+            return Page();
         }
         public IActionResult OnPost([FromRoute] String id)
         {
