ba7cf366c41e74437408e86a11c5314a360ecd81
Katarina4444 <76787593+Katarina4444@users.noreply.github.com>
2021-02-13 00:51:56 +0100

    ...

--- BazeApoteka/BazeApoteka/Pages/KupiLek.cshtml.cs
+++ BazeApoteka/BazeApoteka/Pages/KupiLek.cshtml.cs
@@ -23,24 +23,42 @@ namespace BazeApoteka.Pages
         public IMongoCollection<Lek> collectionL { get; set; }
         [BindProperty]
         public List<Lek> lekovi { get; set; }
-        public IActionResult OnGet()
+        public IActionResult OnGet([FromRoute] String id)
         {
             var connectionString = "mongodb://localhost/?safe=true";
             var client = new MongoClient(connectionString);
             var database = client.GetDatabase("Apoteka3");
 
             collectionL = database.GetCollection<Lek>("lekovi");
-            collectionK = database.GetCollection<Korisnik>("korisnici");
+           // collectionK = database.GetCollection<Korisnik>("korisnici");
 
             lekovi = collectionL.Find(x => x.Id == Lek.Id).ToList();
-            korisnici = collectionK.Find(x => x.Id == Korisnik.Id).ToList();
+           // korisnici = collectionK.Find(x => x.Id == Korisnik.Id).ToList();
 
             return Page();
         }
         public IActionResult OnPost([FromRoute]String id)
         {
             Lek.Id = ObjectId.Parse(id);
-            Korisnik.BrojZdravstveneKnjizice = float.Parse(id);
+            //Korisnik.BrojZdravstveneKnjizice = float.Parse(id);
+
+            var connectionString = "mongodb://localhost/?safe=true";
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase("Apoteka3");
+
+            collectionL = database.GetCollection<Lek>("lekovi");
+            //collectionK = database.GetCollection<Korisnik>("korisnici");
+
+            lekovi = collectionL.Find(x => x.Id == Lek.Id).ToList();
+            //korisnici = collectionK.Find(x => x.BrojZdravstveneKnjizice == Korisnik.BrojZdravstveneKnjizice).ToList();
+
+            return Page();
+        }
+
+        public IActionResult OnPostKupi()
+        {
+            
+            //Korisnik.BrojZdravstveneKnjizice = float.Parse(id);
 
             var connectionString = "mongodb://localhost/?safe=true";
             var client = new MongoClient(connectionString);
@@ -51,6 +69,24 @@ namespace BazeApoteka.Pages
 
             lekovi = collectionL.Find(x => x.Id == Lek.Id).ToList();
             korisnici = collectionK.Find(x => x.BrojZdravstveneKnjizice == Korisnik.BrojZdravstveneKnjizice).ToList();
+
+            Lek lek = lekovi.FirstOrDefault();
+
+            if (lek.DaLiJeNaRecept == "da")
+            {
+                //ako je na recept proveri da li postoji recept kod korisnika
+            }
+            else
+            {
+                if (int.Parse(lek.Kolicina)>0)
+                {
+                    int novaKolicina = int.Parse(lek.Kolicina) - 1;
+                    lek.Kolicina = novaKolicina.ToString();
+                    var res = Builders<Lek>.Filter.Eq(pd => pd.Id, lek.Id);
+                    var operation = Builders<Lek>.Update.Set(u => u.Kolicina, lek.Kolicina);
+                    database.GetCollection<Lek>("lekovi").UpdateOne(res, operation);
+                }
+            }
 
             return Page();
         }
