65a5f25018af66261316aee8efd1a95995028258
Vidosava98 <74729669+Vidosava98@users.noreply.github.com>
2021-02-14 20:19:18 +0100

    svelekove
    svelekove

--- BazeApoteka/BazeApoteka/Pages/UcitajSveLekove.cshtml.cs
+++ BazeApoteka/BazeApoteka/Pages/UcitajSveLekove.cshtml.cs
@@ -14,18 +14,33 @@ namespace BazeApoteka.Pages
     public class UcitajSveLekoveModel : PageModel
     {
         public IMongoCollection<Lek> collection { get; set; }
+        public IMongoCollection<Apoteka> collection2 { get; set; }
         [BindProperty]
         public List<Lek> lekovi { get; set; }
+        [BindProperty]
+        public Apoteka apoteka { get; set; }
+        [BindProperty]
+        public List<Apoteka> apoteke { get; set; }
+        [BindProperty]
+        public List<Apoteka> apoteke2 { get; set; }
         public void OnGet()
         {
             var connectionString = "mongodb://localhost/?safe=true";
             var client = new MongoClient(connectionString);
             var database = client.GetDatabase("Apoteka3");
-
-
             collection = database.GetCollection<Lek>("lekovi");
 
             lekovi = collection.Find(FilterDefinition<Lek>.Empty).ToList();
+
+            collection2 = database.GetCollection<Apoteka>("apoteke");
+
+            apoteke = collection2.Find(FilterDefinition<Apoteka>.Empty).ToList();
+
+            foreach (Lek l in lekovi)
+            {
+                Apoteka a = collection2.Find(x => x.Id == l.MojaApoteka.Id).FirstOrDefault();
+                apoteka.Naziv = a.Naziv;
+            }
         }
         public IActionResult OnPost()
         {
@@ -36,6 +51,21 @@ namespace BazeApoteka.Pages
 
             collection = database.GetCollection<Lek>("lekovi");
             lekovi = collection.Find(FilterDefinition<Lek>.Empty).ToList();
+            collection2 = database.GetCollection<Apoteka>("apoteke");
+
+            apoteke = collection2.Find(FilterDefinition<Apoteka>.Empty).ToList();
+
+            foreach (Lek l in lekovi)
+            {
+
+                Apoteka a = collection2.Find(x => x.Id == l.MojaApoteka.Id).FirstOrDefault();
+                //apoteka.Naziv = a.Naziv;
+                apoteke2.Add(a);
+            }
+
+
+
+
             return Page();
         }
     }
